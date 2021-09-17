c8f11e0f3e157203a68bf11a5392ea683429a898
Ruan <karlosruan9@gmail.com>
2021-09-17 11:07:51 -0300

    trabalhando com templates string

---
+++ numeros/numeros.js
@@ -0,0 +1,26 @@
+
+let numero = 9999999999999999 //numero inteiro permite um tamanaho de até 15 numeros
+let flutuante = 0.2+0.1//numero decimal pode ter até 17 numero depois da virgula
+
+let x = 1, y = 2, z='3', k='4', a='a', b='b'
+let res = a / x
+
+console.log(x+y)//retorna um inteiro
+console.log(a+b)//faz uma concatenação, e retorna um string
+
+console.log('A soma é ' + x + y)//javascript interpreta da esquerda para a direita
+
+console.log(y*k)//javascrip irá converter sempre que possivel um string em um inteiro
+
+console.log(z*k)
+
+console.log(a/x)//retorna um not a number
+
+console.log(isNaN(res))//retorna true se for um not a number
+
+console.log(typeof(NaN))//not a number é do tipo number
+
+let num = 10
+
+console.log(num.toString(2))//toString serve também para converter para uma base
+
---
+++ strings/template_string.js
@@ -0,0 +1,12 @@
+
+let texto = `Hello Word !!`
+let nome = `Ruan`, sobrenome = `carlos`
+
+let inteiro = 100
+let flutuante = 1.11
+
+console.log(`Seu nome é ${nome} é o sobrenome é ${sobrenome}`)
+
+
+console.log(`${(inteiro*flutuante).toFixed(3)}`)
+
